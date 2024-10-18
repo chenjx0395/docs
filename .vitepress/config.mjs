7a8ca84bcae59c27c2734a36950eac0ad3b8495f
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-10-18 16:24:30 +0800

    Initial commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,103 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from '../utils/auto_sidebar.mjs'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+
+  // 地址栏logo
+  head :[["link",{rel: "icon", href: "/public/logo.svg"}]],
+
+  // 主页小标题
+  title: "陳序源个人文档库",
+ // SEO 描述
+  description: "编程 java 项目 前端 鸿蒙 HarmonyOS 计算机 计算机网络 数据库",
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // logo 配置
+    logo: '/public/logo.svg',
+    // 导航栏配置
+    nav: [
+      // 无下拉框
+      { text: '主页', link: '/' },
+      // 有下拉框
+      { text: '项目', items:[
+        { text: '星星交友', link: '/'},
+        { text: '彗星AI刷题平台', link: '/'},
+        { text: '恒星API调度平台', link: '/'},
+      ]},
+      { text: '后端', items:[
+        { text: 'Java核心语法', link: '/backend/java_basic'},
+        { text: 'MySQL', link: '/backend/mysql'},
+      ]}
+    ],
+
+    // 右侧导航栏设置
+    // 导航栏标题
+    outlineTitle: "文章目录",
+    // 导航栏接受的 md 标题级别
+    outline: [1,6],
+
+    // 修改左侧侧边栏，也是可有下拉配置
+    /* sidebar: [
+
+      {
+        text: '项目',
+        items: [
+          { text: '星星交友', link: '/markdown-examples' },
+          { text: '彗星AI刷题平台', link: '/api-examples' },
+          { text: '恒星API调度平台', link: '/api-examples' },
+        ]
+      },
+      {
+        text: '后端',
+        items: [
+          { text: 'Java核心语法', link: '/markdown-examples' },
+          { text: 'MySQL数据库', link: '/api-examples' },
+          { text: 'Redis', link: '/api-examples' },
+        ]
+      },
+
+    ], */
+    // 脚本自动生成侧边栏
+    sidebar: { 
+      "/backend/java_basic": set_sidebar("/backend/java_basic"), 
+      "/backend/mysql": set_sidebar("/backend/mysql"), 
+    },
+
+
+    // 最右侧的社交链接
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/chenjx0395' }
+    ],
+
+    // 配置搜索栏
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档"
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换"
+            }
+          }
+        }
+      }
+    },
+
+    // 底部自定义内容
+    footer:{
+      copyright: "Copyright @ 2024-present 陳序源"
+    }
+
+
+
+  }
+})
---
+++ index.md
@@ -0,0 +1,39 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  # 主页标题
+  name: "陳序源个人文档库"
+  # 主页描述
+  text: "记录编程学习产生的文档&笔记"
+  # 标语 -- 更小的文本
+  tagline: 人本自然，无为而生
+  # 背景图
+  image: 
+    # 读取的是public内的文件。原因：未知
+    src: background.svg
+    alt: background
+  # 按钮
+  actions:
+    # 按钮样式
+    - theme: brand
+      # 按钮文本内容
+      text: 项目
+      # 点击后跳转的页面
+      link: https://www.baidu.com
+    - theme: alt
+      text: 笔记
+      link: /api-examples
+# 块状的文本区域
+features:
+  # 文本域标题
+  - title: 星星交友
+    # 文本域描述
+    details: 致力于打造以兴趣和目标为驱动的交友平台
+  - title: 彗星AI刷题平台
+    details: 智慧化刷题造题平台，为创造减负
+  - title: 恒星API调度平台
+    details: 提供高性能的API调用服务
+---
+
